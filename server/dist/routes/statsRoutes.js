717f92557050ad87dd1b65306aded4c6c48509a3
mac <mac@macs-MacBook-Pro.local>
2026-08-28 21:55:54 +0500

    fix: add .gitignore, untrack node_modules and dist, add root vercel.json configuration

--- server/dist/routes/statsRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const statsController_1 = require("../controllers/statsController");
-const auth_1 = require("../middleware/auth");
-const router = (0, express_1.Router)();
-router.get('/admin', auth_1.protect, auth_1.adminOnly, statsController_1.getAdminStats);
-exports.default = router;
